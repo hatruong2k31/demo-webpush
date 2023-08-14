3e687af3ab280906e1092659825d4b4def4bdb40
hqtruong <hatruong2k31@gmail.com>
2023-08-14 17:39:09 +0700

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyCaIKBIfi3OO5FqUtW7kRBvP4NWqyq2qUI",
   authDomain: "test-fcm-a1205.firebaseapp.com",
   projectId: "test-fcm-a1205",
@@ -7,24 +7,58 @@ const firebaseConfig = {
   appId: "1:408902584117:web:ee6d2dcda80c7473746e7c",
   measurementId: "G-WT2CJ81E9J",
 };
-
-firebase?.initializeApp(firebaseConfig);
-
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging
-  .requestPermission()
-  .then(function () {
-    console.log("Quyền thông báo đã được cấp!");
-    return messaging.getToken({
-      vapidKey:
-        "BGQkvie7tjIuh3b8D-HOTE3-W7oCDfcRnLMI9RpVPrmk72jjcH7yfs6EdTnR0iJFsXQYN9V0MIzP-Yzyh1jofxA",
+function IntitalizeFireBaseMessaging() {
+  messaging
+    .requestPermission()
+    .then(() => {
+      console.log("Quyền thông báo đã được cấp!");
+      return messaging.getToken({
+        vapidKey:
+          "BGQkvie7tjIuh3b8D-HOTE3-W7oCDfcRnLMI9RpVPrmk72jjcH7yfs6EdTnR0iJFsXQYN9V0MIzP-Yzyh1jofxA",
+      });
+    })
+    .then(function (token) {
+      console.log("Token : " + token);
+      document.getElementById("token").innerHTML = token;
+    })
+    .catch(function (reason) {
+      console.log(reason);
     });
-  })
-  .then(function (currentToken) {
-    console.log("Token : " + currentToken);
-    // Gửi token này lên máy chủ để lưu lại và sử dụng khi gửi thông báo.
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+}
+
+messaging.onMessage(function (payload) {
+  console.log(payload);
+  const notificationOption = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  if (Notification.permission === "granted") {
+    var notification = new Notification(
+      payload.notification.title,
+      notificationOption
+    );
+
+    notification.onclick = function (ev) {
+      ev.preventDefault();
+      window.open(payload.notification.click_action, "_blank");
+      notification.close();
+    };
+  }
+});
+
+messaging.onTokenRefresh(function () {
+  messaging
+    .getToken()
+    .then(function (newtoken) {
+      console.log("New Token : " + newtoken);
+    })
+    .catch(function (reason) {
+      console.log(reason);
+    });
+});
+
+IntitalizeFireBaseMessaging();
