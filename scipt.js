607ba691e89ff314263a7e6d73b110b8646e8a3e
hqtruong <hatruong2k31@gmail.com>
2023-11-15 11:27:47 +0700

    update sw and script

--- scipt.js
+++ scipt.js
@@ -33,24 +33,7 @@ function IntitalizeFireBaseMessaging() {
 }
 
 messaging.onMessage((payload) => {
-  console.log(payload);
-  const notificationOption = {
-    body: payload.notification.body,
-    icon: payload.notification.icon,
-  };
-
-  if (Notification.permission === "granted") {
-    var notification = new Notification(
-      payload.notification.title,
-      notificationOption
-    );
-
-    notification.onclick = function (ev) {
-      ev.preventDefault();
-      window.open(payload.notification.click_action, "_blank");
-      notification.close();
-    };
-  }
+  console.log("payload", payload);
 });
 
 messaging.onTokenRefresh(() => {
