4b7eabc289304cd4f22eb9cf2366f50c5006f3a6
hqtruong <hqtruong@whitespace.vn>
2023-12-14 13:45:04 +0700

    update script

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-
+import logo from "./logo.svg";
 const firebaseConfig = {
   apiKey: "AIzaSyCCSMhwQDwknF6JTpbVdilygUg7pyEyOGI",
   authDomain: "webpushnotification-18248.firebaseapp.com",
@@ -11,28 +11,53 @@ const firebaseConfig = {
   measurementId: "G-CL1SN2102X",
 };
 
-Notification?.requestPermission()
-  .then((permission) => {
-    if (permission === "granted") {
-      console.log("Quyền thông báo đã được cấp!!!");
-      const app = initializeApp(firebaseConfig);
-      const messaging = getMessaging(app);
-      getToken(messaging, {
-        vapidKey:
-          "BCy4EVfXHpIdK-unBIYieIfSBngDBPWwjsngkNz57UxW4N-ahPUgVgGY99fLIfIWTwIj1ZKeGi2yOJIP5Y9Rw28",
-      })
-        .then((currentToken) => {
-          if (currentToken) {
-            console.log("token: ", currentToken);
-          } else console.log("get token err");
+function IntitalizeFireBaseMessaging() {
+  Notification?.requestPermission()
+    .then((permission) => {
+      if (permission === "granted") {
+        console.log("Quyền thông báo đã được cấp!!!");
+
+        const app = initializeApp(firebaseConfig);
+        const messaging = getMessaging(app);
+        getToken(messaging, {
+          vapidKey:
+            "BCy4EVfXHpIdK-unBIYieIfSBngDBPWwjsngkNz57UxW4N-ahPUgVgGY99fLIfIWTwIj1ZKeGi2yOJIP5Y9Rw28",
         })
-        .catch((err) => {
-          console.log(err);
+          .then((currentToken) => {
+            if (currentToken) {
+              console.log("token: ", currentToken);
+            } else console.log("get token err");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        onMessage(messaging, (payload) => {
+          console.log("payload ", payload);
+
+          const title = payload.data.title;
+          const options = {
+            body: payload.data.body,
+            data: { url: payload.data.click_link },
+            icon: logo, // Dev vita đưa logo vita vào đây ..........................
+            image: payload.data.image,
+          };
+          if (Notification.permission === "granted") {
+            var notification = new Notification(title, options);
+            notification.onclick = function (event) {
+              event.preventDefault();
+              window.open(payload.data.click_link, "_blank");
+              notification.close();
+            };
+          }
         });
-    } else {
-      console.log("Không được cấp quyền thông báo!");
-    }
-  })
-  .catch((err) => {
-    console.log("Err", err);
-  });
+      } else {
+        console.log("Không được cấp quyền thông báo!");
+      }
+    })
+    .catch((err) => {
+      console.log("Err", err);
+    });
+}
+
+IntitalizeFireBaseMessaging();
